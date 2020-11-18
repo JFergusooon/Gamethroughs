c6bd146d6369e7a26f505a07e058db7c097883c7
Jeff Ferguson <JFergusooon@gmail.com>
2020-11-18 10:50:54 -0700

    Project Creation

---
+++ src/main/java/ferguson/jeffrey/walkthroughproject/ServletInitializer.java
@@ -0,0 +1,13 @@
+package ferguson.jeffrey.walkthroughproject;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(WalkthroughProjectApplication.class);
+	}
+
+}
